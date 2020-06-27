0d1c2c59dbabe5d4c07f9d28f45b7f768316e530
Kailash Sharma <61358755+kailash1011@users.noreply.github.com>
2020-06-27 17:15:20 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name = "travel_india",
+      version = '1.0',
+      description = "An GUI for Bus Booking Software",
+      url = "https://github.com/kailash1011/travel_india",
+      author = "Kailash Sharma",
+      author_email = "kailashps.1011@gmail.com",
+      license = "MIT",
+
+      classifiers = ['Programming Language :: Python :: 3.6'],
+      install_requires = ['tkinter','tkcalendar', 'random','datetime']
+
+      )
